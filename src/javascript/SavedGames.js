2d07e439761d3ca51f5b85a60c720485f826d86d
Silvervingen <antta839@student.liu.se>
2023-05-16 11:31:43 +0200

    fixat savedgames
    Vi har nu gjort det möjligt att lägga till spela från gameinfo and att man kan titta på fyra olika bestämda listoroch lägga in spel i dem.

--- src/javascript/SavedGames.js
+++ src/javascript/SavedGames.js
@@ -13,10 +13,12 @@ function SavedGames() {
   const [games, setGames] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [savedGames, setSavedGames] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("rating");
+  const [selectedList, setSelectedList] = useState("list1"); // Track the selected list
 
   useEffect(() => {
     const count = 1000;
-    const url = `https://api.rawg.io/api/games?key=${apiKey}&metacritic=50,100&ratings_count_min=1000&exclude_additions=true&rating_min=2.5&search=${searchInput}`;
+    const url = `https://api.rawg.io/api/games?key=${apiKey}&metacritic=50,100&ordering=-${selectedOption}&ratings_count_min=1000&exclude_additions=true&rating_min=2.5&search=${searchInput}`;
     setGames([]); // reset games state to empty array, so its is possible to change order mulitple times
     fetch(url)
       .then(res => res.json())
@@ -24,14 +26,18 @@ function SavedGames() {
         setGames(data.results);
       })
       .catch(error => console.error('Error:', error));
-  }, [searchInput]);
+  }, [selectedOption, searchInput]);
 
   useEffect(() => {
-    // get saved games from database for the current user
+    // Get saved games from the database for the current user and selected list
     const userId = auth.currentUser.uid;
-    db.collection("savedGames")
-      .where("userId", "==", userId)
-      .get()
+    let query = db.collection("savedGames").where("userId", "==", userId);
+    
+    if (selectedList !== null) {
+      query = query.where("listId", "==", selectedList); // Filter by the selected list
+    }
+
+    query.get()
       .then((querySnapshot) => {
         const savedGames = [];
         querySnapshot.forEach((doc) => {
@@ -42,76 +48,136 @@ function SavedGames() {
       .catch((error) => {
         console.error("Error getting saved games:", error);
       });
-  }, []);
+  }, [selectedList]);
 
   const addToSavedGames = (game) => {
     const userId = auth.currentUser.uid;
-    const query = db.collection("savedGames").where("userId", "==", userId).where("id", "==", game.id);
-    query.get().then((querySnapshot) => {
-      if (querySnapshot.size === 0) {
-        // game doesn't exist in savedGames collection, so add it
-        db.collection("savedGames")
-          .add({ ...game, userId })
-          .then(() => {
-            console.log("Game added to saved games");
-            setSavedGames([...savedGames, { ...game, userId }]); // update saved games state
-          })
-          .catch((error) => {
-            console.error("Error adding game to saved games:", error);
-          });
-      } else {
-        // game exists in savedGames collection, so remove it
-        querySnapshot.docs[0].ref.delete()
-          .then(() => {
-            console.log("Game removed from saved games");
-            setSavedGames(savedGames.filter(savedGame => savedGame.id !== game.id)); // update saved games state
-          })
-          .catch((error) => {
-            console.error("Error removing game from saved games:", error);
-          });
-      }
-    });
+    const query = db.collection("savedGames").where("userId", "==", userId).where("listId", "==", selectedList).where("id", "==", game.id);
+  
+    query
+      .get()
+      .then((querySnapshot) => {
+        if (querySnapshot.size === 0) {
+          // Game doesn't exist in savedGames collection, so add it
+          db.collection("savedGames")
+            .add({ ...game, userId, listId: selectedList }) // Assign the selected list to the game
+            .then(() => {
+              console.log("Game added to saved games");
+              const updatedSavedGames = [
+                ...savedGames,
+                { ...game, userId, listId: selectedList },
+              ];
+              setSavedGames(updatedSavedGames); // Update saved games state
+            })
+            .catch((error) => {
+              console.error("Error adding game to saved games:", error);
+            });
+        } else {
+          // Game exists in savedGames collection, so remove it
+          querySnapshot.docs[0].ref
+            .delete()
+            .then(() => {
+              console.log("Game removed from saved games");
+              const updatedSavedGames = savedGames.filter(
+                (savedGame) =>
+                  savedGame.id !== game.id || savedGame.listId !== selectedList
+              );
+              setSavedGames(updatedSavedGames); // Update saved games state
+            })
+            .catch((error) => {
+              console.error("Error removing game from saved games:", error);
+            });
+        }
+      });
   };
   
-  
-  return (
-    <div>
-      <div className="Library-header">
-        <h1>Lists</h1>
-        <div id="searchbar2">
-          <input id="searchbar" type="text" placeholder="Search.." value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
-        </div>
-      </div>
-  
-      {games.length > 0 && searchInput !== "" && (
-  <div className="game-container">
-    {games.map((game) => (
-      <div className="game-box" key={game.id}>
-        <img src={game.background_image} alt={game.name} />
-        <h2>{game.name}</h2>
-        <p>Rating: {game.rating}</p>
-        <button onClick={() => {addToSavedGames(game); setSearchInput("")}}>
-          {savedGames.some(savedGame => savedGame.id === game.id) ? "Remove from saved games" : "Add to saved games"}
-        </button>
-      </div>
-    ))}
+
+  const handleListChange = (event) => {
+    const newListId = event.target.value;
+    setSelectedList(newListId);
+    };
+    
+    return (
+    <div className="Listpage">
+    
+
+  <div className="saved-games-container">
+    <h2>Saved Games</h2>
+    <div className="list-buttons">
+  <div>
+    <button
+      onClick={() => handleListChange({ target: { value: "list1" } })}
+      className={selectedList === "list1" ? "active" : "list"}
+    >
+      Games I Have
+    </button>
+    <button
+      onClick={() => handleListChange({ target: { value: "list2" } })}
+      className={selectedList === "list2" ? "active" : "list"}
+    >
+      Games I Want
+    </button>
+    <button
+      onClick={() => handleListChange({ target: { value: "list3" } })}
+      className={selectedList === "list3" ? "active" : "list"}
+    >
+      Games Completed
+    </button>
+    <button
+      onClick={() => handleListChange({ target: { value: "list4" } })}
+      className={selectedList === "list4" ? "active" : "list"}
+    >
+      Games I Hate
+    </button>
   </div>
-)}
-  
-      <div className="saved-games-container">
-        <h2>Saved Games</h2>
-        {savedGames.map((game) => (
-          <div className="gameBox" key={game.id}>
+</div>
+
+<div className="sgLibrary-header">
+    <div id="sgsb2">
+    <input id="sgsb" type="text" placeholder="Search for games to add to selected list" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+    <select name="Filter" id="Filter" value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
+      <option value="rating" selected>Rating</option>
+      <option value="released">Newest</option>
+      <option value="Popular">Popular</option>
+    </select>
+    </div>
+    </div>
+    {games.length > 0 && searchInput !== "" && (
+    <div className="game-container">
+      {games.map((game) => (
+        <div className="game-boxes" key={game.id}>
+          <img src={game.background_image} alt={game.name} />
+          <h2>{game.name}</h2>
+          <p>Rating: {game.rating}</p>
+          <button className="remove" onClick={() => { addToSavedGames(game); setSearchInput(""); }}>
+            {savedGames.some(savedGame => savedGame.id === game.id) ? "Remove from saved games" : "Add to saved games"}
+          </button>
+        </div>
+      ))}
+    </div>
+  )}
+<div className="list-container">
+    {savedGames.length > 0 ? (
+      savedGames.map((game) => (
+        <div className="gameBox" key={game.id}>
+          <Link to={`/GameInfo/${game.id}`} key={game.id} className="Links" game={game}>
             <img src={game.background_image} alt={game.name} />
             <h2>{game.name}</h2>
             <p>Rating: {game.rating}</p>
-            <button onClick={() => {addToSavedGames(game); setSearchInput("")}}>
-              Remove from saved games
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-  
-}export default SavedGames;
+          </Link>
+          <button className="SavedButton" onClick={() => { addToSavedGames(game); setSearchInput(""); }}>
+            Remove from saved games
+          </button>
+        </div>
+      ))
+    ) : (
+      <p>No saved games found.</p>
+    )}
+  </div>
+</div>
+</div>
+);
+
+}
+
+export default SavedGames;
--- src/pages/GameInfo.js
+++ src/pages/GameInfo.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../css/GameInfo.css";
+import { db, auth } from "../javascript/firestore";
+
+
 
 import ps5Image from '../pictures/PS5.png'
 import ps4Image from '../pictures/PS4.png'
@@ -58,6 +61,11 @@ function GameInfo() {
   const { id } = useParams();
   const [screenshots, setScreenshots] = useState([]);
 
+  const [games, setGames] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [savedGames, setSavedGames] = useState([]);
+  const [selectedList, setSelectedList] = useState("list1"); // Track the selected list
+
 
   useEffect(() => {
     fetch(`https://api.rawg.io/api/games/${id}?key=${apiKey}`)
@@ -72,6 +80,74 @@ function GameInfo() {
   
   }, [id]);
 
+  useEffect(() => {
+    // Get saved games from the database for the current user and selected list
+    const userId = auth.currentUser.uid;
+    let query = db.collection("savedGames").where("userId", "==", userId);
+    
+    if (selectedList !== null) {
+      query = query.where("listId", "==", selectedList); // Filter by the selected list
+    }
+
+    query.get()
+      .then((querySnapshot) => {
+        const savedGames = [];
+        querySnapshot.forEach((doc) => {
+          savedGames.push(doc.data());
+        });
+        setSavedGames(savedGames);
+      })
+      .catch((error) => {
+        console.error("Error getting saved games:", error);
+      });
+  }, [selectedList]);
+
+  const addToSavedGames = (game) => {
+    const userId = auth.currentUser.uid;
+    const query = db.collection("savedGames").where("userId", "==", userId).where("listId", "==", selectedList).where("id", "==", game.id);
+  
+    query
+      .get()
+      .then((querySnapshot) => {
+        if (querySnapshot.size === 0) {
+          // Game doesn't exist in savedGames collection, so add it
+          db.collection("savedGames")
+            .add({ ...game, userId, listId: selectedList }) // Assign the selected list to the game
+            .then(() => {
+              console.log("Game added to saved games");
+              const updatedSavedGames = [
+                ...savedGames,
+                { ...game, userId, listId: selectedList },
+              ];
+              setSavedGames(updatedSavedGames); // Update saved games state
+            })
+            .catch((error) => {
+              console.error("Error adding game to saved games:", error);
+            });
+        } else {
+          // Game exists in savedGames collection, so remove it
+          querySnapshot.docs[0].ref
+            .delete()
+            .then(() => {
+              console.log("Game removed from saved games");
+              const updatedSavedGames = savedGames.filter(
+                (savedGame) =>
+                  savedGame.id !== game.id || savedGame.listId !== selectedList
+              );
+              setSavedGames(updatedSavedGames); // Update saved games state
+            })
+            .catch((error) => {
+              console.error("Error removing game from saved games:", error);
+            });
+        }
+      });
+  };
+
+  const handleListChange = (event) => {
+    const newListId = event.target.value;
+    setSelectedList(newListId);
+    };
+
   return (
     <div class="Info-container">
         <div class="gameContainer">
@@ -79,17 +155,33 @@ function GameInfo() {
         <h1 id="gameName">{game.name}</h1>
       <h4>Rating: {game.rating}</h4>
       <h4>Released: {game.released}</h4>
+      <label htmlFor="list-select">Select a list:</label>
       
+         <select id="list-select" onChange={handleListChange} value={selectedList}>
+           <option value="list1">Games I have</option>
+           <option value="list2">Games I want</option> {/* Replace with your list names and IDs */}
+           <option value="list3">Games completed </option> {/* Replace with your list names and IDs */}
+           <option value="list4">Games i hate </option> {/* Replace with your list names and IDs */}
+         </select>
+
+         {auth.currentUser && (
+  <button onClick={() => addToSavedGames(game)}>
+    {savedGames.some(
+      (savedGame) =>
+        savedGame.listId === selectedList && savedGame.id === game.id)
+      ? "Remove from saved games" // If game already exists in the list
+      : "Add to saved games"}
+  </button>
+)}
+
       <h2>Description</h2>
       <p>{game.description_raw}</p>
-      
       <h2>Platforms</h2>
       {game.platforms?.map((platform) => (
         <img class='platformLogo' src={platformImages[platform.platform.name]} alt={platform.platform.name} />
-    ))}
-
+    ))} 
+    
     </div>
-   
     <div class="gameContainer2">
     <div className="game-screenshots">
           {screenshots.map(screenshot => (
@@ -98,7 +190,7 @@ function GameInfo() {
         
         </div>
    </div>
-    
+   
 	<button className = "Next" onClick={() => window.history.back()}>Go Back</button>
   
 </div>
